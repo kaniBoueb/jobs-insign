840392eb3b82f4278244e33e2199fdbdbccd0b64
kaniBoueb <kani.bouebassihou@insign.africa>
2024-01-09 13:13:24 +0000

    Fix bug: Ajout offre

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\OffreController;
+use App\Http\Controllers\FrontEndController;
 use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
