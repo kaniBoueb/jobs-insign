5ea9215da19c3bd2c0ad2456c62c90b5401a8d74
kaniBoueb <kani.bouebassihou@insign.africa>
2024-01-10 16:28:57 +0000

    Update UI Gestion Offre

--- resources/views/offre/index.blade.php
+++ resources/views/offre/index.blade.php
@@ -3,8 +3,11 @@
 @section('content')
     <div class="container">
         @if (count($offres)> 0)
-            <h2>Liste des offres </h2> 
-            <table class="table">
+            <div class="head-page d-flex justify-content-between py-3">
+                <h2>Liste des offres </h2>
+                <a href="{{ route('offre.create')}}" class="btn text-light" id="new" style="background: #14214c;">Créer une offre</a>
+            </div>
+            <table class="table my-2">
                 <thead>
                     <tr>
                         <th scope="col">#</th>
@@ -23,8 +26,8 @@
                             <td style="border: 1px solid #00000000; border-top-left-radius: 50px; border-bottom-left-radius: 50px; text-align: center; color: #fff; background: #14214c; padding: 0 20px; margin-right: 30px" scope="row"> {{ $offre->id }} </td>
                             <td> {{ $offre->reference_offre }} </td>
                             <td> {{ $offre->titre }} </td>
-                            <td> {{ $offre->date_emission }} </td>
-                            <td> {{ $offre->date_echeance }} </td>
+                            <td> <span class="badge bg-dark">{{ $offre->date_emission }}</span> </td>
+                            <td> <span class="badge bg-warning">{{ $offre->date_echeance }} </span> </td>
                             <td></td>
                             <td style="background: #14214c; justify-content: center; align-items: center; border-top-right-radius: 50px; border-bottom-right-radius: 50px; border: 1px solid #00000000;">
                                 <a href="{{ route('offre.edit', $offre->id)}}" id="edit"><i class="fa fa-pen text-light"></i></a>
